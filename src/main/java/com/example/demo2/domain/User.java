cf0b75e3b4b548a6e017eb3d6d88304dcea21263
Toida <tetsuro.toida@rakus-partners.co.jp>
2025-01-27 17:28:39 +0900

    second commit

---
+++ src/main/java/com/example/demo2/domain/User.java
@@ -0,0 +1,25 @@
+package com.example.demo2.domain;
+
+public class User {
+    private String name;
+    private String comment;
+
+    public User(String name2, String comment2) {
+        //TODO Auto-generated constructor stub
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getComment() {
+        return comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+   
+
+}
